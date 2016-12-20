bf872143588698412d4d16ef9cf806f266e5a3e9
zagpro1408 <zagorskybobuyc@gmail.com>
2016-12-20 20:00:03 +0300

    Update add_to_cart function

--- public/app.js
+++ public/app.js
@@ -5,12 +5,14 @@ function something() {
     var x = window.localStorage.getItem('bbb')
 
     x = x * 1 + 1;
-
     window.localStorage.setItem('bbb', x);
-
     alert(x);
 }
 
 function add_to_cart(id) {
-    alert('You added pizza with id: ' + id)
+    var key = 'product_' + id;
+
+    var x = window.localStorage.getItem(key);
+    x = x * 1 + 1;
+    window.localStorage.setItem(key, x);
 }
