9ae6069303ac982229a23b54cf8e1014584e4911
zagpro1408 <zagorskybobuyc@gmail.com>
2016-12-21 10:32:13 +0300

    Add items counter into cart button

--- public/app.js
+++ public/app.js
@@ -17,6 +17,7 @@ function add_to_cart(id) {
     window.localStorage.setItem(key, x);
 
     update_orders_input();
+    update_orders_button();
 }
 
 function cart_get_number_of_items() {
@@ -36,6 +37,11 @@ function cart_get_number_of_items() {
 function update_orders_input() {
     var orders = cart_get_orders();
     $('#orders_input').val(orders);
+}
+
+function update_orders_button() {
+    var text = 'Cart (' + cart_get_number_of_items() + ')';
+    $('#orders_button').val(text);
 }
 
 function cart_get_orders() {
